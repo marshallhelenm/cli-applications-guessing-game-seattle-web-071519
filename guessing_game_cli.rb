5078b6e70f141ab1b5c927c2c584e43054553da2
Helen Marshall <marshallhelenm@gmail.com>
2019-07-10 15:59:12 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -6,5 +6,7 @@ def run_guessing_game
     puts "Goodbye!"
   elsif input == num 
     puts "You guessed the correct number!"
+  elsif input != num 
+    puts "Sorry! The computer guessed #{num}."
   end
 end
